d0ab5a34af6600e72285dc73ef6f17fb74d2e167
lChen-dev <louis.chen2117@gmail.com>
2022-10-31 15:25:54 -0400

    Completed following all of the tutorial.

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,57 @@
+import react.*
+import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.h3
+import react.dom.html.ReactHTML.div
+
+val App = FC<Props> {
+    var currentVideo: Video? by useState(null)
+    var unwatchedVideos: List<Video> by useState(listOf(
+        Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+        Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+        Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+    ))
+    var watchedVideos: List<Video> by useState(listOf(
+        Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+    ))
+    h1 {
+        +"Hello, React+Kotlin/JS!"
+    }
+    div {
+        h3 {
+            +"Videos to watch"
+        }
+        VideoList {
+            videos = unwatchedVideos
+            selectedVideo = currentVideo
+            onSelectVideo = { video ->
+                currentVideo = video
+            }
+        }
+
+        h3 {
+            +"Videos watched"
+        }
+        VideoList {
+            videos = watchedVideos
+            selectedVideo = currentVideo
+            onSelectVideo = { video ->
+                currentVideo = video
+            }
+        }
+    }
+    currentVideo?.let { curr ->
+        VideoPlayer {
+            video = curr
+            unwatchedVideo = curr in unwatchedVideos
+            onWatchedButtonPressed = {
+                if (video in unwatchedVideos) {
+                    unwatchedVideos = unwatchedVideos - video
+                    watchedVideos = watchedVideos + video
+                } else {
+                    watchedVideos = watchedVideos - video
+                    unwatchedVideos = unwatchedVideos + video
+                }
+            }
+        }
+    }
+}
